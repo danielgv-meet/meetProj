3f6206eeefc755b583c449a58e6e3263f48d4ab9
danielgv-meet <startech5674@gmail.com>
2024-07-30 15:08:15 +0300

    dadsa

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ import pyrebase
 import firebase
 
 firebaseConfig = {
-  "apiKey": "AIzaSyB057zr6FS31fUqXSuvPuGwTgf4CU3_MNM",
-  "authDomain": "auth-lab-1349a.firebaseapp.com",
-  "projectId": "auth-lab-1349a",
-  "storageBucket": "auth-lab-1349a.appspot.com",
-  "messagingSenderId": "916347876212",
-  "appId": "1:916347876212:web:aa74c8b48d817af88fb221",
-  "databaseURL": "https://project2-b06a5-default-rtdb.europe-west1.firebasedatabase.app/"
+    "apiKey":
+    "AIzaSyB057zr6FS31fUqXSuvPuGwTgf4CU3_MNM",
+    "authDomain":
+    "auth-lab-1349a.firebaseapp.com",
+    "projectId":
+    "auth-lab-1349a",
+    "storageBucket":
+    "auth-lab-1349a.appspot.com",
+    "messagingSenderId":
+    "916347876212",
+    "appId":
+    "1:916347876212:web:aa74c8b48d817af88fb221",
+    "databaseURL":
+    "https://project2-b06a5-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
@@ -21,85 +27,117 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+
 @app.route("/")
 def main_page():
-	return render_template('main.html')
+  return render_template('main.html')
+
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
-	if request.method == 'POST':
-		email = request.form['email']	
-		password = request.form['password']
-		name = request.form['name']
-		Y_O = request.form['school']
-		user = {"name": name, "email": email, "school": Y_O}
-		print(user)
-		try:
-			if Y_O == "Y" :
-				login_session['user'] = auth.create_user_with_email_and_password(email, password)
-				UID = login_session["user"]["localId"]
-				db.child("Users").child(UID).set(user)
-				
-				return redirect(url_for("home"))
-			else:
-				login_session['user'] = auth.create_user_with_email_and_password(email, password)
-				UID = login_session["user"]["localId"]
-				db.child("user_o").child(UID).set(user)
+  if request.method == 'POST':
+    email = request.form['email']
+    password = request.form['password']
+    name = request.form['name']
+    Y_O = request.form['school']
+    user = {
+        "name": name,
+        "email": email,
+        "school": Y_O,
+        "school_name": '',
+        "hobbies": ''
+    }
+    print(user)
+    try:
+      if Y_O == "Younited":
+        login_session['user'] = auth.create_user_with_email_and_password(
+            email, password)
+        UID = login_session["user"]["localId"]
+        db.child("Users").child(UID).set(user)
 
-			
-		
-			return redirect(url_for("home"))
-		except Exception as e:
-			print(e)
-			return "error"
-	else:
-		return render_template('signup.html')
+        return redirect(url_for("home"))
+      else:
+        login_session['user'] = auth.create_user_with_email_and_password(
+            email, password)
+        UID = login_session["user"]["localId"]
+        db.child("user_o").child(UID).set(user)
+
+      return redirect(url_for("home"))
+    except Exception as e:
+      print(e)
+      return "error"
+  else:
+    return render_template('signup.html')
+
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
-	if request.method == "POST":
-		email = request.form["email"]
-		password = request.form['password']
-		try:
-			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-			return redirect(url_for("home"))
-		except:
-			print("The user does not exist")
-			return render_template("error.html", error = "the user does not exist")
-	else:
-		return render_template('signin.html')
+  if request.method == "POST":
+    email = request.form["email"]
+    password = request.form['password']
+    try:
+      login_session['user'] = auth.sign_in_with_email_and_password(
+          email, password)
+      return redirect(url_for("home"))
+    except:
+      print("The user does not exist")
+      return render_template("error.html", error="the user does not exist")
+  else:
+    return render_template('signin.html')
+
 
 @app.route("/profiles", methods=["GET", "POST"])
 def profiles():
-	if request.method == "POST":
-		users_id = request.form['id']
-		return render_template('profile.html', id = users_id, users = db.child('Users').get().val())
-	else:
-		return render_template('profiles.html', users = db.child('Users').get().val())
+  if request.method == "POST":
+    users_id = request.form['id']
+    UID = login_session['user']['localId']
+    return render_template('profile.html',
+                           id=users_id,
+                           users=db.child('Users').get().val(),
+                           user_id=UID)
+  else:
+    return render_template('profiles.html',
+                           users=db.child('Users').get().val())
 
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
-	if request.method == "POST":
-		return render_template('home.html')
-	else:
-		return render_template('home.html')
+  if request.method == "POST":
+    return render_template('home.html')
+  else:
+    return render_template('home.html')
+
 
 @app.route("/your_profile")
 def your_porfile():
-	UID = login_session['user']['localId']
-	if UID in db.child('Users').get().val():
-		return render_template('profile.html', id=UID, users=db.child('Users').get().val())
-	else:
-		return render_template('profile.html', id=UID, users=db.child('user_o').get().val())
+  UID = login_session['user']['localId']
+  if UID in db.child('Users').get().val():
+    return render_template('profile.html',
+                           id=UID,
+                           users=db.child('Users').get().val(), user_id = UID)
+  else:
+    return render_template('profile.html',
+                           id=UID,
+                           users=db.child('user_o').get().val(), user_id= UID)
+
 
 @app.route("/signout")
 def signout():
-	login_session["user"].clear()
-	return redirect('/')
+  login_session["user"].clear()
+  return redirect('/')
 
-#First Pull
+@app.route('/edit_profile', methods=['GET', 'GET'])
+def edit_profile():
+  if request.method == "POST":
+    school_name = request.form['school']
+    hobbies = request.form['hobbies']
+    name = request.form['name']
+    UID = login_session['user']['localId']
+    if UID in db.child('Users'):
+      if 
+      db.child('Users').child(UID).update({'name': name, 'school_name': school_name, "hobbies": hobbies})
+      
+      
 
 if __name__ == "__main__":
- 	app.run(debug=True)
-    
+  app.run(debug=True)
